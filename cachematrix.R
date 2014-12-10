2d5c1b539d6ed6820dfc5ee9be00fda7976c61ec
dhyvc <forgithub2014@gmail.com>
2014-12-10 18:44:53 +0200

    0.1

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file includes a pair of functions for calculating
+## the inverse of a matrix, either by returning a previous
+## cached value, or calculating it and caching it for 
+## future use.
 
-## Write a short comment describing this function
-
-## Testing 123
+## This function creates a set of helper functions for
+## supporting caching the calculation of the inverse of
+## a matrix. These functions include:
+##
+## 1. Setting the matrix in the global environment
+## 2. Getting the matrix from the global environment
+## 3. Setting the value of the matrix inverse
+## 4. Getting the value of the matrix inverse
+##
+## The function returns a list of the above functions
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
